0c9731c8fc212a1ff909af511f66871ea9317a0c
Martin Treml <github@r2pi.net>
2018-01-06 19:26:18 +0100

    Initial commit to test travis push

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ ! -f /config/settings.conf ]; then
+    echo "No configuration file found proceeding with default values"
+    python3 /app/configurator.py
+else
+    echo "Configuration file found starting configurator with provided settings"
+    python3 /app/configurator.py /config/settings.conf
+fi
